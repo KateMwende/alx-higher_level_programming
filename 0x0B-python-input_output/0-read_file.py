398eb3a6714345994d7ad2e1e2d5586745fb4709
Katemwende <kmwende419@gmail.com>
2023-02-13 18:17:30 +0000

    Executable

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that reads a text file (UTF8) and prints it to stdout"""
 
+
 def read_file(filename=""):
     """Function that reads a file and prints to stdout
     Args:
