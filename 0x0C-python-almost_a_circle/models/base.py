68e53d35d4f81e744eae784afc869fb7bdd998b4
Katemwende <kmwende419@gmail.com>
2023-02-16 08:55:05 +0000

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyhoon3
+"""Base class module"""
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class initialization
+        Args:
+            id: public instance attribute
+        """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+        else:
+            self.id = id
