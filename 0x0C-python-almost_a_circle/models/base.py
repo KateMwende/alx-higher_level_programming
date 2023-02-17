009c2d8c7cee77384bd1ffb2963dbf496d2e60c3
Katemwende <kmwende419@gmail.com>
2023-02-17 20:49:21 +0000

    Json String format

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
         else:
             self.id = id
 
-    @static.method
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
 
