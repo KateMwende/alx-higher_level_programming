8a8160e7aee39867814a7d580ef7f15b454e3274
Katemwende <kmwende419@gmail.com>
2023-03-14 21:58:03 +0000

    file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates a table second_table
 -- Query that creates second_table
-CREATE TABLE IF NOT EXIST second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 id INT,
 name VARCHAR(256),
 score INT)
