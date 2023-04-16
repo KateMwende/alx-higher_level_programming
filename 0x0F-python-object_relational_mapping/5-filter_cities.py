4d78bd587faf61475817d67ff4e07e6012a7992d
Katemwende <kmwende419@gmail.com>
2023-04-16 15:12:41 +0000

    Cities within a state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    states.name = sys.argv[4]
+    cur.execute("SELECT cities.name FROM cities JOIN states\
+                 ON cities.states_id=states.id")
+    cities = cur.fetchall()
+    for city in cities:
+        print(', '.join(city))
