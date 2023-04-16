aa88eca0cf6fc14a4cca23dfeb3a837588119c4b
Katemwende <kmwende419@gmail.com>
2023-04-16 20:36:26 +0000

    Print State object with the name passed as argument

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     session = Session()
     search = sys.argv[4]
 
-    state = session.query(State)
-    if (search == State.name):
+    state = session.query(State).filter(State.name = sys.argv[4])
+    if search:
         print("{}".format(state.id))
     else:
         print('Not found')
