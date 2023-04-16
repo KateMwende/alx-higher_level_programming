628191a541f42700d2358b2dd9447afe2a73aa33
Katemwende <kmwende419@gmail.com>
2023-04-16 11:09:58 +0000

    Argument to match list

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,12 @@ table of hbtn_0e_0_usa where name matches the argument
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], search=sys.argv[4])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     state = cur.fetchall()
-    if sys.argv[4] === state:
-        print(state)
+    for state in states:
+        if sys.argv[4] == state:
+            print(state)
