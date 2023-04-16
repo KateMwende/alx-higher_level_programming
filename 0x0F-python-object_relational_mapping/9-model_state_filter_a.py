89a34dcd02722c13b7eda868facbc17ab113bd61
Katemwende <kmwende419@gmail.com>
2023-04-16 20:15:14 +0000

    Print list oject having letter 'a'

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,11 +11,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).filter_by(name='a')
+    states = session.query(State).order_by(State.id).filter_by(State.name='a')
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
