2385a56ccde21b0af004716d973a69adcc641ec8
Katemwende <kmwende419@gmail.com>
2023-04-16 13:38:36 +0000

    Joinig id's to print cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name
-                FROM cities JOIN states ON cities.states_id=states.id")
+                FROM cities JOIN states ON cities.state_id=states.id")
     cities = cur.fetchall()
     for city in cities:
         print(city)
