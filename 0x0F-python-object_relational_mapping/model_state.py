4551a388ed71f5c87817909cb600c940191e3127
Katemwende <kmwende419@gmail.com>
2023-04-16 18:03:05 +0000

    Added table name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,11 +4,12 @@ class State and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+
+iBase = declarative_base()
 
 
 class states(Base):
     """Defines a class state to link to a table"""
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary key=true)
+    id = Column(Integer, unique=True, nullable=False, primary key=True)
     name = Column(String(128), nullable=False)
