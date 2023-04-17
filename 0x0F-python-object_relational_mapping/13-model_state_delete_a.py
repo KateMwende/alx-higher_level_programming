7f5de50e6e91335c16c87329f1725e6f1450fb43
Katemwende <kmwende419@gmail.com>
2023-04-17 11:45:45 +0000

    Delete a state

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Delete state with 'a'
+"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    stateUpdate = session.query(State)
+    if 'a' in stateUpdate:
+        session.delete(stateUpdate)
+        session.commit()
