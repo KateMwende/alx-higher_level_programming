ee0af0753cd3622a45491fa82f548f532fbdb359
Katemwende <kmwende419@gmail.com>
2023-04-17 15:31:01 +0000

    Class City and printing city objects

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    p = session.query(City, State).filter(City.state_id == State.id).all()
+    p = session.query(City, State).filter(City.state_id == State.id)\
+        .order_by(City.id)
 
     for city, state in p:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
