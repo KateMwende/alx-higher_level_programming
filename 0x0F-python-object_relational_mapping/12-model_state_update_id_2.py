5bf2699d97076e92bf5755af6b29b1ad88b859d7
Katemwende <kmwende419@gmail.com>
2023-04-17 11:28:23 +0000

    Update a state with id of 2 to New Mexico

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = craete_engine("mysql+mysqldb://{}:{}@localhost:3306:{}".
+    engine = craete_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
