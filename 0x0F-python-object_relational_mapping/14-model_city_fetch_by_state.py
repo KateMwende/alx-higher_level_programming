0296dfa268b74df0b37a8dd5076f36dfffd34424
Katemwende <kmwende419@gmail.com>
2023-04-17 12:33:15 +0000

    Class City and printing city objects

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    p = session.query(City, State).order_by(City.state.id=State.idi).all()
+
+    for city, state in p:
+        print("{}: {} {}".format(state.name, city.id, city.name))
