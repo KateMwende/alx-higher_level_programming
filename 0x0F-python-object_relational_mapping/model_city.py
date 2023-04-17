d7c16e417512aec894a5acfc48fb6f3d07acb0ad
Katemwende <kmwende419@gmail.com>
2023-04-17 12:41:58 +0000

    Class City and printing city objects

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """
 class definition of a City
 """
-from sqlalchemy import Column, Integer, String, Foreignkey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative.base()
@@ -15,4 +15,4 @@ class City(Base):
     __table__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128))
-    state_id = Column(Integer, nullable=False, Foreignkey('states.id'))
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
