1741a9979417b0712f1c292f13de4935025b3a69
Katemwende <kmwende419@gmail.com>
2023-05-03 16:35:17 +0000

    Url and email arguments

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
 
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(email).encode("ascii")
+
+    req = urllib.request.Request(url, email)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
