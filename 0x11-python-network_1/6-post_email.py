00810ab5e3b439691e0b9d395f60201eb872df3f
Katemwende <kmwende419@gmail.com>
2023-05-03 17:39:29 +0000

    Use requests with 2 parameteres

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {"Email": sys.argv[2]}
-    r = requests.post(url, email)
+    email = {"email": sys.argv[2]}
+    r = requests.post(url, data=email)
     print(r.text)
