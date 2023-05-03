77bd31676d1d3f3af2033d5eee549522be943192
Katemwende <kmwende419@gmail.com>
2023-05-03 16:51:50 +0000

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,6 +3,9 @@
 script that takes in a URL, sends a request to the
 URL and displays the body of the response (decoded in utf-8).
 """
+import sys
+import urllib.request
+import urllib.error
 
 
 if __name__ == "__main__":
