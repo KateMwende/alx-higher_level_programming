53669607a6f85f5d5cae407d17f050e6f26a6984
Katemwende <kmwende419@gmail.com>
2023-05-04 02:39:02 +0000

    Username and password arguments

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+    r = response.json()
+    try:
+        print(j.get("id"))
+    except Exception:
+        print("None")
